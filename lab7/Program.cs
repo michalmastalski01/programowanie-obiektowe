c39c6b11bddc3b4996f769c28e12335e4a73aa96
michal.mastalski <michal.mastalski@gmail.com>
2022-04-27 18:28:16 +0200

    Zakończenie pracy lab8

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -1,66 +1,297 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace lab7
+delegate String IntFormatter(int a);
+
+delegate double operation(double a, double b);
+
+class Program
 {
-    delegate double Operator(double a, double b);
-    class Program
+    public static void Main(string[] args)
     {
-
-        public static double Addition(double x, double y)
+        int points = 0;
+        try
         {
-            return x + y;
-        }
-        public static double Mul(double x, double y)
-        {
-            return x * y;
-        }
-        public static void PrintIntArray (int[] arr, Func<int, string> formatter)
-        {
-            foreach(var item in arr)
+            IntFormatter formatter = HexFormatter();
+            if (formatter.Invoke(15).Equals("F") && formatter.Invoke(5).Equals("5") && formatter.Invoke(11).Equals("B"))
             {
-                Console.WriteLine(formatter.Invoke(item));
+                Console.WriteLine("Zadanie 1: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 1: 0");
             }
         }
-        static void Main(string[] args)
+        catch (NotImplementedException e)
         {
-            Operator operation = Addition;
-            //równoważnik Addition(4, 6);
-            double result = operation.Invoke(4, 6);
-            Console.WriteLine(result);
-            operation = Mul;
-            result = operation.Invoke(4, 6);
-            Console.WriteLine(result);
-
-            Func<double, double, double> op = Mul;
-            Func<int, string> Formatter = delegate (int number)
-            {
-                return string.Format("0x{0:x}", number);
-            };
-            Func<int, string> DecFormat = delegate (int number)
-            {
-                return string.Format("{0}", number);
-            };
-
-            Console.WriteLine(Formatter.Invoke(23));
-
-            Predicate<string> OnlyThreeChars = delegate (string s)
-            {
-                return s.Length == 3;
-            };
-            Func<int, int, int, bool> InRange = delegate (int value, int min, int max)
-            {
-                return value > min && value < max;
-            };
-
-            Action<string> Print = delegate (string s)
-            {
-                Console.WriteLine(s);
-            };
-            Operator AddLambda = (a, b) => a + b;
-            Action<string> PrintLambda = s => Console.WriteLine(s);
-            Func<int> Lambda = () => 3;
-            PrintIntArray(new int[] { 1, 5, 23, 533 }, n => string.Format("{0}", n));
-            PrintIntArray(new int[] { 1, 5, 23, 533 }, DecFormat);
+            Console.WriteLine("Zadanie 1: 0");
         }
+        try
+        {
+            operation op = AddOperation();
+            if (op is operation && op.Invoke(1, 2) == 3 && op.Invoke(100, 3) == 103)
+            {
+                Console.WriteLine("Zadanie 2: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 2: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 2: 0");
+        }
+        try
+        {
+            double result = Calculate(delegate (double a, double b) { return a * b; }, 2, 4);
+            if (result == 8)
+            {
+                Console.WriteLine("Zadanie 3: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 3: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 3: 0");
+        }
+        try
+        {
+            string result = Repeat().Invoke("AA", 3);
+            if (result.Equals("AAAAAA") && Repeat().Invoke("-", 2).Equals("--") && Repeat().Invoke("", 3).Equals(""))
+            {
+                Console.WriteLine("Zadanie 4: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 4: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 5: 0");
+        }
+        try
+        {
+            if (StringConsumer() is Action<string>)
+            {
+                Console.WriteLine("Zadanie 5: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 5: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 5: 0");
+        }
+        try
+        {
+            if (DoubleFunction() is Func<double, double> && DoubleFunction().Invoke(4) == 16 && DoubleFunction().Invoke(8) == 64)
+            {
+                Console.WriteLine("Zadanie 6: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 6: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 6: 0");
+        }
+        try
+        {
+            if (IsPhoneNumber() is Predicate<string> && IsPhoneNumber().Invoke("123456789") && !IsPhoneNumber().Invoke("44567ad") && !IsPhoneNumber().Invoke("12345678967"))
+            {
+                Console.WriteLine("Zadanie 7: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 7: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 7: 0");
+        }
+        try
+        {
+            List<string> data = new List<string>()
+            {
+                "123456789 90",
+                "272899987 87",
+                "-72899987 87",
+                "111234bn2 90",
+                "272899987 -3",
+                "83935 0"
+
+            };
+            if (ProcessPeople(data) != null && ProcessPeople(data).Contains(new Person("123456789", 90))
+                && ProcessPeople(data).Contains(new Person("272899987", 87))
+                )
+            {
+                Console.WriteLine("Zadanie 8: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 8: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 8: 0");
+        }
+        try
+        {
+            Messenger messenger = new Messenger();
+
+            if (MessangerSubsciber(messenger, "") != null && MessangerSubsciber(messenger, "Test").Equals("Test"))
+            {
+                Console.WriteLine("Zadanie 9: 1");
+                points++;
+            }
+            else
+            {
+                Console.WriteLine("Zadanie 9: 0");
+            }
+        }
+        catch (NotImplementedException e)
+        {
+            Console.WriteLine("Zadanie 9: 0");
+        }
+
+        Console.WriteLine("Suma punktów: " + points);
+    }
+
+    //Zadanie 1 
+    //w metodzie uzupełnij ciało delegata, aby zwracał łańcuch z liczbą `value` zapisaną w kodzie szesnastkowym
+    //np. dla value 10 powinien zwrócić "A"
+    public static IntFormatter HexFormatter()
+    {
+        return delegate (int value)
+        {
+            return string.Format("{0:X}", value);
+        };
+    }
+    //Zadanie 2
+    //zwróć delegata typu operation, który dodaje oba argumenty 
+    public static operation AddOperation()
+    {
+        return delegate (double x, double y)
+        {
+            return x + y;
+        };
+    }
+
+    //Zadanie 3
+    //wywołaj przekazanego delegata op z parametrami a i b, a wynik delegata zwróć jako wartość metody Calculate
+    public static double Calculate(operation op, double a, double b)
+    {
+        return op(a, b);
+    }
+
+    //Zadanie 4
+    //Zwróć wartość delegata typu Func, który zwraca powtórzony łańcuch (pierwszy argument) n razy (drugi argument) 
+    public static Func<string, int, string> Repeat()
+    {
+        return delegate (string s, int x)
+        {
+            string result = "";
+            for (int i = 0; i < x; i++)
+            {
+                result += s;
+            }
+            return result;
+        };
+    }
+
+    //Zadanie 5
+    //zwroć w metodzie lambdę, która wyświetla na konsoli przekazany łańcuch wielkimi literami
+    public static Action<string> StringConsumer()
+    {
+        return s => s.ToUpper();
+    }
+    //Zadanie 6
+    //zwroć w metodzie lambdę, która zwraca argument podniesiony do kwadratu
+    public static Func<double, double> DoubleFunction()
+    {
+        return square => Math.Pow(square, 2);
+    }
+    //Zadanie 7
+    //zwróć w metodzie lambdę, która zwraca prawdę, jeśli argument jest poprawnym numerem telefonu:
+    //- ma 9 znaków
+    //- każdy znak jest cyfrą
+    public static Predicate<string> IsPhoneNumber()
+    {
+        int i = 0;
+        return s => s.Length == 9 && int.TryParse(s, out i);
+    }
+    public static List<Person> LoadPeople(List<String> RawData, Predicate<string> validator)
+    {
+        List<Person> list = new List<Person>();
+        foreach (var row in RawData)
+        {
+            string[] tokens = row.Split(" ");
+            string phone = tokens[0];
+            string ects = tokens[1];
+            if (validator != null && validator.Invoke(row))
+            {
+                list.Add(new Person(phone, int.Parse(ects)));
+            }
+        }
+        return list;
+    }
+
+    //Zadanie 8
+    //Podaj w miejscu null lambdę predykatu, która validuje argument wejsciowy w postaci łańcucha
+    //łańcuch składa się z dwóch części oddzielonych spacją
+    //pierwsza zawiera nr telefonu (9 cyfr)
+    //druga zwiera liczbę całkowitą puktów Ects, która nie może być ujemna 
+    //jeśli obie części zawierają poprawne dane to predykat zwarac true
+    public static List<Person> ProcessPeople(List<String> data)
+    {
+        return LoadPeople(data, null);
+    }
+
+    //Zadanie 9
+    //Zdefiniuj słuchacza klasy Messanger w postaci lambdy, który po odbiorze wiadomości przypisze ją do zmiennej lokalnej receivedMessage
+    public static string MessangerSubsciber(Messenger messenger, String message)
+    {
+        string receivedMessage = null;
+        //poniżej wpisz lambdę, która jest subskrybentem obiektu messanger
+        messenger.SendToAll(message);
+        return receivedMessage;
     }
 }
+record Person(string Phone, int Ects);
+
+
+class Messenger
+{
+    public event EventHandler<string> BrodcastMessage;
+
+    protected virtual void OnBroadcast(string message)
+    {
+        BrodcastMessage?.Invoke(this, message);
+    }
+
+    public void SendToAll(string message)
+    {
+        OnBroadcast(message);
+    }
+}
+
+
---
+++ lab8/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace lab8
+{
+    record Student(string Name, char Team, int Ects);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 5, 3, 1, 7, 4, 7, 9, 5, 2 };
+            Predicate<int> oddPredicate = n =>
+            {
+                Console.WriteLine("Obliczanie predykatu dla n " + n);
+                return n % 2 == 1;
+            };
+            Console.WriteLine("Przed wykonaniem wiersza 1");
+            IEnumerable<int> odds = from n in ints
+            where oddPredicate.Invoke(n)
+            select n;
+            Console.WriteLine("Przed wykonaniem wiersza 2");
+            odds = from n in odds
+            where n > 5
+            select n;
+            //rozbudowany predykat
+            int limit = 5;
+            odds = from n in ints
+            where n % 2 == 1 && n > limit
+            select n;
+            Console.WriteLine(string.Join(", ", odds));
+            string[] strings = {"aa", "bb", "ccc", "fff", "ee", "ggggg"};
+            //zapisz wyrażenie linq ktore zwroci liste lancuchow o dlugosci 2 znakow
+            IEnumerable<int> znaki = from s in strings
+                                     select s.Length;
+            Console.WriteLine(string.Join(", ", znaki));
+            znaki = from s in strings
+                    orderby s descending
+                    select s.Length;
+            Console.WriteLine(string.Join(", ", znaki));
+
+            Console.WriteLine(string.Join(", ", from n in ints orderby n descending select Math.Pow(n, 2)));
+            Student[] students =
+            {
+                new Student("Jacek", 'A', 89),
+                new Student("Anna", 'A', 76),
+                new Student("Konstantyn", 'B', 104),
+                new Student("Wawrzyniec", 'A', 64),
+                new Student("Piotr", 'B', 81),
+                new Student("Anna", 'A', 95)
+            };
+            Console.WriteLine(string.Join(", ", from s in students group s by s.Team into team select (team.Key, team.Count())));
+            IEnumerable<IGrouping<char, Student>> teams = from s in students group s by s.Team;
+            foreach(var item in teams)
+            {
+                Console.WriteLine("Grupa: " + item.Key + "| " + string.Join("\n", item));
+            }
+            string sentence = "ala ma kota ala lubi koty tomek lubi psy";
+            //wykonaj zestawienie ile razy każdy z wyrazów występuje w sentence
+            string[] tab = sentence.Split(" ");
+            Console.WriteLine(string.Join(", ", from t in tab group t by t into wyrazy select (wyrazy.Key, wyrazy.Count())));
+
+            IEnumerable<int> enumerable = ints
+                .Where(n => n % 2 == 1)
+                .OrderBy(n => n)
+                .Select(n => (int)Math.Pow(n, 2));
+            Console.WriteLine(string.Join(", ", enumerable));
+            students.GroupBy(student => student.Team).Select(gr => (gr.Key, gr.Count()));
+            IOrderedEnumerable<Student> sorted = students.OrderBy(student => student.Name).ThenByDescending(student => student.Ects);
+            Console.WriteLine(string.Join("\n", sorted));
+
+            //posortuj łańcuchy w strings wg dlugosci a łańcuchy o tej samej dlugosci rosnąco alfabetycznie.
+            Console.WriteLine(string.Join("\n",
+                strings.OrderBy(s => s.Length).ThenBy(s => s)
+                ));
+            int wynik = Enumerable.Range(0, 10).Where(n => n % 2 == 0).Sum();
+            //suma kwadratów liczb nieparzystych od 0 do 10
+            int wynik2 = Enumerable.Range(0, 11).Where(n => n % 2 == 0).Select(n => (int)Math.Pow(n, 2)).Sum();
+            Console.WriteLine(wynik2);
+            Student second = students.OrderByDescending(s => s.Ects).ElementAt(1);
+            Console.WriteLine(second);
+            Student st = students.FirstOrDefault(s => s.Name.StartsWith("A"));
+            Console.WriteLine(st);
+
+            Random random = new Random();
+            random.Next(5);
+            //wygenerować tablicę 100 liczb losowych o wartościach od 0 do 9.
+            
+        }
+    }
+}
